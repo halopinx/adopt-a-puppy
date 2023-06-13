c7693b2caf586c81709dbb72daf16201232bfd58
Errah <errah@ypdigital.com.au>
2023-06-13 23:31:06 +0800

    update filter to useReducer

--- src/pages/PuppyList.js
+++ src/pages/PuppyList.js
@@ -1,120 +1,96 @@
-import { useState, useEffect } from 'react'
+import { useReducer} from 'react'
 import PuppyCard from "../features/puppy-comps/PuppyCard";
 import Input from '../components/ui/form/Input';
+import Button from '../components/ui/Button';
 import classes from './PuppyList.module.scss'
 import { DUMMY_DATA } from "../dummy-data";
 
-const PuppyListPage = () => {
-    const [query, setQuery] = useState('');
-    const [filteredData, setFilteredData] = useState(DUMMY_DATA);
 
-    useEffect(() => {
-        setFilteredData(
-            DUMMY_DATA.filter(data => 
-                data.name.toLowerCase().includes(query) ||
-                data.age === +query  ||
-                data.breed.toLowerCase().includes(query) ||
-                data.gender.includes(query)
-           )
+const initialState = {
+    queries: '',
+    age: '',
+    breed: '',
+    gender: '',
+    data: [...DUMMY_DATA]
+}
+
+const queryReducer = (state, action) => {
+    if (action.type === 'SEARCH') {
+        return { ...state, queries: action.value.toLowerCase(), data: [...action.data]}
+    }
+    if (action.type === 'SEARCH_AGE') {
+        return { ...state,  age: action.value, data: [...action.data] }
+    }
+    if (action.type === 'SEARCH_BREED') {
+        return { ...state,  breed: action.value.toLowerCase(), data: [...action.data] }
+    }
+    if (action.type === 'SEARCH_GENDER') {
+        return { ...state,  gender: action.value.toLowerCase(), data: [...action.data] }
+    }
+    if (action.type === 'RESET') {
+        return initialState;
+    }
+    return state;
+}
+
+const PuppyListPage = () => {
+    const [queryState, dispatch] = useReducer(queryReducer, initialState);
+
+    const filterSearch = (target) => {
+        return initialState.data.filter(data => 
+            data.name.toLowerCase().includes(target) || 
+            data.breed.toLowerCase().includes(target) || 
+            data.age.toString() === target || 
+            data.gender.toLowerCase() === target
         )
-    }, [query])
+    }
 
    
-   const ages = DUMMY_DATA.map( data => data.age);
+   const ages = initialState.data.map( data => data.age);
    const minAge = Math.min(...ages);
    const maxAge = Math.max(...ages);
-   const breedArr = DUMMY_DATA.map(data => data.breed).filter((breed, i, arr) => arr.indexOf(breed) === i );
-
-   const reset = () => setFilteredData(DUMMY_DATA);
+   const breedArr = initialState.data.map(data => data.breed).filter((breed, i, arr) => arr.indexOf(breed) === i );
 
    const searchHandler = (e) => {
-        if (e.target.value === '') reset();
-        setQuery(e.target.value)
+        dispatch({type: 'SEARCH', value: e.target.value, data: filterSearch(e.target.value)})
     }
 
    const ageFilterHandler = (e) => {
-        const queried = [...DUMMY_DATA.filter(data => 
-            data.name.toLowerCase().includes(query) ||
-            data.age === +query  ||
-            data.breed.toLowerCase().includes(query) ||
-            data.gender.includes(query)
-       )];
-
-        if (e.target.value === '' && query === '') return reset();
-        
-
-        if (query === '') {
-            return setFilteredData(DUMMY_DATA.filter(data => data.age === +e.target.value));
-        }
-
-        if (e.target.value === '' && query) return setFilteredData(queried)
-
-        return setFilteredData(queried.filter(data => data.age === +e.target.value))
+        dispatch({type: 'SEARCH_AGE', value: e.target.value, data: filterSearch(e.target.value)})
    }
 
    const breedFilterHandler = (e) => {
-        const queried = [...DUMMY_DATA.filter(data => 
-            data.name.toLowerCase().includes(query) ||
-            data.age === +query  ||
-            data.breed.toLowerCase().includes(query) ||
-            data.gender.includes(query)
-        )]
-
-        console.log('queried breed', queried)
-
-        if (e.target.value === 'all' && query === '') return reset();
-
-        if (query === '') {
-            return setFilteredData(DUMMY_DATA.filter(data => data.breed.toLowerCase() === e.target.value));
-        }
-
-        if (e.target.value === 'all' && query) return setFilteredData(queried)
-
-        return setFilteredData(queried.filter(data => data.breed.toLowerCase() === e.target.value))
-
+        dispatch({type: 'SEARCH_BREED', value: e.target.value, data: filterSearch(e.target.value)})
    }
 
-
     const genderFilterHandler = (e) => {
-        const queried = [...DUMMY_DATA.filter(data => 
-            data.name.toLowerCase().includes(query) ||
-            data.age === +query  ||
-            data.breed.toLowerCase().includes(query) ||
-            data.gender.includes(query)
-       )]
-        console.log('queried gender', queried)
-        if (e.target.value === 'all' && query === '') return reset();
-
-        if (query === '') {
-            return setFilteredData(DUMMY_DATA.filter(data => data.gender.toLowerCase() === e.target.value));
-        }
-
-        if (e.target.value === 'all' && query ) return setFilteredData(queried)
-
-        return setFilteredData(queried.filter(data => data.gender === e.target.value))
+        dispatch({type: 'SEARCH_GENDER', value: e.target.value, data: filterSearch(e.target.value)})
     }
+
+    const resetFilterHandler = () => dispatch({ type: 'RESET' })
 
     return ( 
         <div className="app-container">
              <div className={classes.wrapper}>
                 <aside className={classes.filters}>
                         <h3>Filter puppy</h3>
-                        <Input type="search" placeholder="Search puppy..." onChange={searchHandler}/>
-                        <Input type='number' min={minAge} max={maxAge} onChange={ageFilterHandler} placeholder='All ages' label='Age' />
-                        <Input variant='select' name='breed' onChange={breedFilterHandler} label='Breed'>
-                            <option value="all">All Breed</option>
+                        <Input type="search" placeholder="Search puppy..." onChange={searchHandler} value={queryState.queries}/>
+                        <Input type='number' min={minAge} max={maxAge} onChange={ageFilterHandler} placeholder='All ages' label='Age' value={queryState.age} />
+                        <Input variant='select' name='breed' onChange={breedFilterHandler} label='Breed' value={queryState.breed}>
+                            <option value="">All Breed</option>
                             {
                                 breedArr.map((breed, index) => <option key={index} value={breed.toLowerCase()}>{breed}</option>)
                             }
                         </Input>
-                        <Input variant='select' name='gender' onChange={genderFilterHandler} label='Gender'>
-                            <option value="all">All Gender</option>
+                        <Input variant='select' name='gender' onChange={genderFilterHandler} label='Gender' value={queryState.gender}>
+                            <option value="">All Gender</option>
                             <option value="male">Male</option>
                             <option value="female">Female</option>
                         </Input>
+                        <Button variant='button' onClick={resetFilterHandler} className={classes.reset}>Clear Filters</Button>
                 </aside>
                 <div className={classes.results}>
-                        {filteredData.map(data => {
+                        {queryState.data.map(data => {
                             const link = `/${data.name}-${data.id}`.toLowerCase();
                             return (
                                 <PuppyCard 
